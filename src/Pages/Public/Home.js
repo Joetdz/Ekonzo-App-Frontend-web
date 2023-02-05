898f41692c8cd560ff2ab1b1eec26e00c23e4b1a
Joel tondozi <joeltondozi@gmail.com>
2023-02-06 00:50:16 +0100

    feat : create eparge whith objectif section

--- src/Pages/Public/Home.js
+++ src/Pages/Public/Home.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react"
 import { useState } from "react"
-import { BiShowAlt } from "react-icons/bi"
+import { BiShowAlt, BiCustomize } from "react-icons/bi"
 import publicContext from "../../Services/Context.Service"
+import { FaCar, FaMotorcycle } from "react-icons/fa"
+import { GiPiggyBank, GiTeacher, GiClothes } from "react-icons/gi"
+import { FcHome, FcPhoneAndroid } from "react-icons/fc"
 const Home = () => {
   const [ShowSold, setShowSold] = useState(false)
 
@@ -23,7 +26,10 @@ const Home = () => {
         <h4>Solde Total</h4>
       </section>
       <section className="ekz-classique">
-        <h1 className="section-title"> Epargnez des depots predefinit</h1>
+        <h1 className="section-title">
+          {" "}
+          Epargnez avec des cartes au nombre des depots predefinit
+        </h1>
         <div className="cards-container">
           <div className="card">
             <div className="raka-raka"></div>
@@ -37,11 +43,59 @@ const Home = () => {
         </div>
       </section>
       <section className="mayele-na-ekz">
-        <h1 className="section-title">Epargnez pour un Objectif précis</h1>
+        <h1 className="section-title">
+          Epargnez pour un Objectif précis et suivez votre progression
+        </h1>
         <div className="mayele-container">
           <div className="objectif">
-            <span className="icon"></span>
-            <span className="name"></span>
+            <span className="icon color-rose">
+              <GiPiggyBank />
+            </span>
+            <span className="name">Fonds pour les urgences </span>
+          </div>
+          <div className="objectif">
+            <span className="icon">
+              <FcHome />
+            </span>
+            <span className="name">Payer le Loyer</span>
+          </div>
+          <div className="objectif">
+            <span className="icon color-sky">
+              <GiTeacher />
+            </span>
+            <span className="name">Payer le Minerval</span>
+          </div>
+
+          <div className="objectif">
+            <span className="icon color-red">
+              <FaMotorcycle />
+            </span>
+            <span className="name">Achetez une Moto</span>
+          </div>
+          <div className="objectif">
+            <span className="icon color-bleu">
+              <FaCar />
+            </span>
+            <span className="name">Achetez une Voiture </span>
+          </div>
+          <div className="objectif">
+            <span className="icon">
+              <FcPhoneAndroid />
+            </span>
+            <span className="name">Achetez un Téléphone</span>
+          </div>
+          <div className="objectif">
+            <span className="icon color-gris">
+              <GiClothes />
+            </span>
+            <span className="name">Achetez des vêtements</span>
+          </div>
+
+          <div className="objectif">
+            <span className="icon color-pinky">
+              <BiCustomize />
+            </span>
+            <span className="name">Personalisé</span>
           </div>
         </div>
       </section>
