653585ec5c2684f2f8cea8bfba57f41ae8bb203f
joel Tondozi <joeltondozi@gmail.com>
2023-04-19 19:06:49 +0100

    feat : add logout in sidebar

--- src/Components/Public/Sidbar.js
+++ src/Components/Public/Sidbar.js
@@ -1,12 +1,36 @@
-import React from "react"
+import React, { useContext, useState } from "react"
+import { CgLogOut } from "react-icons/cg"
+import publicContext from "../../Services/Context.Service"
+import { TimeServices } from "../../Services/Time.Service"
+import { AccountService } from "../../Services/Account.Service"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
 import { TbHome } from "react-icons/tb"
 import { BsCardChecklist } from "react-icons/bs"
 import { MdOutlinePayments } from "react-icons/md"
 import { GrUserSettings } from "react-icons/gr"
+
 const Sidbar = () => {
+  const { user, userisLogged, setUserIslogged } = useContext(publicContext)
+  const [salutation, setSalutation] = useState(
+    TimeServices.getSalutationBytime()
+  )
+  const navigate = useNavigate()
+  const logout = () => {
+    AccountService.logout()
+    setUserIslogged(false)
+  }
+  useEffect(() => {
+    setSalutation(TimeServices.getSalutationBytime())
+    !userisLogged && navigate("/login")
+
+    console.log(userisLogged)
+  }, [userisLogged, salutation])
+
   return (
     <div className="sidbar">
+      <div className="logo"></div>
       <div className="navigation">
         <div className="group-home">
           <Link to="/home">
@@ -40,6 +64,9 @@ const Sidbar = () => {
             options
           </Link>
         </div>
+      </div>
+      <div className="notif">
+        <CgLogOut onClick={logout} />
       </div>
     </div>
   )
