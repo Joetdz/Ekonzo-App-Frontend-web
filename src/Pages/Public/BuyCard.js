18cf7dea708837a022acde4e1eefa52278fe0220
joel Tondozi <joeltondozi@gmail.com>
2023-04-20 18:40:18 +0100

    feat : add redirection after  buy card

--- src/Pages/Public/BuyCard.js
+++ src/Pages/Public/BuyCard.js
@@ -7,8 +7,29 @@ import "react-toastify/dist/ReactToastify.css"
 import { AccountService } from "../../Services/Account.Service"
 import { useEffect } from "react"
 import AuthLoader from "../../Components/AuthLoader"
+import Box from "@mui/material/Box"
+import { useNavigate } from "react-router-dom"
+
+import Typography from "@mui/material/Typography"
+import Modal from "@mui/material/Modal"
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 340,
+  bgcolor: "background.paper",
+  borderRadius: "12px",
+  boxShadow: 24,
+
+  p: 4,
+}
 
 const BuyCard = () => {
+  const [open, setOpen] = React.useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+
   const notify = (message) => {
     toast(message)
   }
@@ -19,6 +40,7 @@ const BuyCard = () => {
   const resetCardBuyStatus = useChallengeStore(
     (state) => state.resetCardBuyStatus
   )
+
   const postBuyCard = useChallengeStore((state) => state.postBuyCard)
   const isloading = useChallengeStore((state) => state.isloading)
 
@@ -91,67 +113,92 @@ const BuyCard = () => {
       ])
     }
   }
+  const navigate = useNavigate()
   useEffect(() => {
+    if (isloading === true) {
+      handleOpen()
+    }
     if (cardBuyStatus) {
       notify(cardBuyStatus)
       resetCardBuyStatus()
+
+      setTimeout(function () {
+        navigate("/cards")
+      }, 5000)
     }
   }, [cardBuyStatus])
 
   return (
-    <div className="buy-card-page">
-      <div className="card">
-        <img src={challengeCard.image} alt="" />
-      </div>
-      <section className="card-detail-section">
-        <form className="form-buy-card" onSubmit={handleSubmit}>
-          <div className="group-input">
-            <label htmlFor="sel"> Dévise</label>
-            <select name="devise" id="sel" onChange={onchange}>
-              <option>Seletionner...</option>
+    <>
+      <div className="buy-card-page">
+        <div className="card">
+          <img src={challengeCard.image} alt="" />
+        </div>
+        <section className="card-detail-section">
+          <form className="form-buy-card" onSubmit={handleSubmit}>
+            <div className="group-input">
+              <label htmlFor="sel"> Dévise</label>
+              <select name="devise" id="sel" onChange={onchange}>
+                <option>Seletionner...</option>
 
-              <option value="USD">Dollars Américain</option>
-            </select>
-          </div>
-          <div className="group-input">
-            <label htmlFor="mtn">Montant de Départ</label>
-            <select id="mtn" name="montant_depart" onChange={onchange}>
-              <option>selectionner...</option>
-              {deviseSelected.map((montant) => (
-                <option value={montant} name="montant_depart">
-                  {montant}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <div className="group-input">
-            <label htmlFor="sel">Méthode de paiement</label>
-            <select name="operateur" id="sel" onChange={onchange}>
-              {" "}
-              <option>selectionner...</option>
-              <option value="MPESA">Mpesa</option>
-              <option value="ORANGE">Orange money</option>
-              <option value="AIRTEL">Airtel money</option>
-              <option value="AFRICEL">Afrimoney</option>
-            </select>
-          </div>
-          <div className="group-input">
-            <label htmlFor="sel">Numéro</label>
-            <div className="num-input">
-              {" "}
-              +243 <input type="tel" name="numero" onChange={onchange} />
+                <option value="USD">Dollars Américain</option>
+              </select>
             </div>
-          </div>
-          <div className="btn">
-            <button className="login-btn-local">
-              Payer la carte {isloading ? <AuthLoader /> : ""}
-            </button>
-          </div>
-          <ToastContainer />
-        </form>
-      </section>
-    </div>
+            <div className="group-input">
+              <label htmlFor="mtn">Montant de Départ</label>
+              <select id="mtn" name="montant_depart" onChange={onchange}>
+                <option>selectionner...</option>
+                {deviseSelected.map((montant) => (
+                  <option value={montant} name="montant_depart">
+                    {montant}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="group-input">
+              <label htmlFor="sel">Méthode de paiement</label>
+              <select name="operateur" id="sel" onChange={onchange}>
+                {" "}
+                <option>selectionner...</option>
+                <option value="MPESA">Mpesa</option>
+                <option value="ORANGE">Orange money</option>
+                <option value="AIRTEL">Airtel money</option>
+                <option value="AFRICEL">Afrimoney</option>
+              </select>
+            </div>
+            <div className="group-input">
+              <label htmlFor="sel">Numéro</label>
+              <div className="num-input">
+                {" "}
+                +243 <input type="tel" name="numero" onChange={onchange} />
+              </div>
+            </div>
+            <div className="btn">
+              <button className="login-btn-local">
+                {isloading ? <AuthLoader /> : "Payer la carte"}
+              </button>
+            </div>
+            <ToastContainer />
+          </form>
+        </section>
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h4"
+            component="h2"
+          ></Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}></Typography>
+        </Box>
+      </Modal>
+    </>
   )
 }
 
